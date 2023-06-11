675ea7a09d8232cb3cf1b13a86ede1a34be276c8
carlos <carlosest.aguilarr@gmail.com>
2023-06-11 04:32:16 -0600

    Terminando Clase 1 Router: Empleando y conociendo la paqueteria React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/sobre' element={ <Sobre /> } />
+      </Routes>            
+    </Router>
   );
 }
 
 export default App;
+
+/*
+Instalacion de Librerias
+  npm i react-router-dom libreria para poder trabajar con rutas
+  npm i react-icons
+*/
